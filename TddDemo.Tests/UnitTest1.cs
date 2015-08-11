7af4d7ba65a374b204b928b31f5233b493f7f0b8
Manuel Riezebosch <mriezebosch@gmail.com>
2015-08-11 08:54:38 +0200

    lege solution met references

--- TddDemo.Tests/UnitTest1.cs
+++ TddDemo.Tests/UnitTest1.cs
@@ -7,49 +7,8 @@ namespace TddDemo.Tests
     public class UnitTest1
     {
         [TestMethod]
-        public void IbanNotStartingWithNLShouldReturnInvalidCountryCodeWhenValidated()
+        public void TestMethod1()
         {
-            string iban = "XX71INGB0448927934";
-            CheckIban checker = new CheckIban();
-
-            IbanValidationResult result = checker.Validate(iban);
-
-            Assert.AreEqual(IbanValidationResult.InvalidCountryCode, result);
-
-        }
-
-        [TestMethod]
-        public void CorrectIbanShouldReturnTrueWhenValidated()
-        {
-            string iban = "NL71INGB0448927934";
-            CheckIban checker = new CheckIban();
-
-            IbanValidationResult result = checker.Validate(iban);
-
-            Assert.AreEqual(IbanValidationResult.OK, result);
-        }
-
-        [TestMethod]
-        public void IbanWithIncorrectCheckDigitsShouldReturnFalseWhenValidated()
-        {
-            string iban = "NLABINGB0448927934";
-            CheckIban checker = new CheckIban();
-
-            IbanValidationResult result = checker.Validate(iban);
-
-            Assert.AreEqual(IbanValidationResult.InvalidCheckDigits, result);
-        }
-
-        [TestMethod]
-        public void IbanWithLowerCaseCountryCodeShouldReturnOkayWhenValidated()
-        {
-            string iban = "nl71INGB0448927934";
-            CheckIban checker = new CheckIban();
-
-            IbanValidationResult result = checker.Validate(iban);
-
-            Assert.AreEqual(IbanValidationResult.OK, result);
-
         }
     }
 }
