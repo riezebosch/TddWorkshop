a3c79c0622ded4ecd2e18c714ba9c03000a62c12
Manuel Riezebosch <mriezebosch@gmail.com>
2015-10-02 09:03:24 +0200

    NUnit test toegevoegd

---
+++ MinDef.TestDemo.Tests/NUnitTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinDef.TestDemo.Tests
+{
+    [NUnit.Framework.TestFixture]
+    public class NUnitTests
+    {
+        [NUnit.Framework.Test]
+        public void NUnitTest()
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+}
