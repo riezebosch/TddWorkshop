0546401d4f7d3494f25ef67124037781276ac78c
Manuel Riezebosch <mriezebosch@gmail.com>
2016-01-04 14:09:17 +0100

    Basis scenario met check op landcode en lengte.

---
+++ TddDemo.Tests/IbanTests.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TddDemo.Tests
+{
+    [TestClass]
+    public class IbanTests
+    {
+        [TestMethod]
+        public void EenValideNederlandseIbanMetSpaties_BijValideren_GeeftAlsResultaatTrue()
+        {
+            // Arrange
+            var input = "NL86 INGB 0002 4455 88";
+            var iban = new Iban(input);
+
+            bool expected = true;
+
+            // Act
+            bool actual = iban.Validate();
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void EenValideNederlandseIbanZonderSpaties_BijValideren_GeeftTrueTerug()
+        {
+            // Arrange
+            var input = "NL86INGB0002445588";
+            var iban = new Iban(input);
+
+            bool expected = true;
+
+            // Act
+            bool actual = iban.Validate();
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void IbanMetLengteKleinerDan18_BijValideren_GeeftFalseTerug()
+        {
+            // Arrange
+            var input = "NL86INGB000244558";
+            var iban = new Iban(input);
+
+            bool expected = false;
+
+            // Act
+            bool actual = iban.Validate();
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void AndereValideIban_BijValideren_GeeftTrueTerug()
+        {
+            var input = "NL91ABNA0417164300";
+            var iban = new Iban(input);
+
+            var actual = iban.Validate();
+
+            Assert.AreEqual(true, actual);
+        }
+    }
+}
---
+++ TddDemo/Iban.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace TddDemo
+{
+    public class Iban
+    {
+        private string _iban;
+
+        /// <summary>
+        /// Commentaar op de constructor
+        /// </summary>
+        /// <param name="iban">Het hoeft nog niet per se een valide iban te zijn, dat fixen we later.</param>
+        public Iban(string iban)
+        {
+            _iban = iban;
+        }
+
+        private static string RemoveWhitespace(string iban)
+        {
+            return iban.Replace(" ", "");
+        }
+
+        /// <summary>
+        /// Valideert een Nederlandse IBAN volgens ISO.....
+        /// </summary>
+        /// <returns>True voor een valide Nederlandse IBAN en false voor een invalide of niet Nederlands. </returns>
+        public bool Validate()
+        {
+            var iban = RemoveWhitespace(_iban);
+            //NL91ABNA0417164300
+            //NL86INGB0002445588
+            if (ValidateLandCode(iban) &&
+                ValidateLength(iban))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private static bool ValidateLength(string iban)
+        {
+            return iban.Length == 18;
+        }
+
+        private static bool ValidateLandCode(string iban)
+        {
+            return iban.StartsWith("NL");
+        }
+    }
+}
