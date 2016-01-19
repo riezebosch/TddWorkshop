f1efe161f2cea7c292bc4422779463dcfda6ab7e
Manuel Riezebosch <mriezebosch@gmail.com>
2016-01-19 10:25:22 +0100

    test on correct iban and test on empty string

---
+++ TddDemo.Tests/IbanValidatorTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TddDemo.Tests
+{
+    [TestClass]
+    public class IbanValidatorTests
+    {
+        [TestMethod]
+        public void ValidIbanShouldReturnTrue()
+        {
+            // Arrange
+            string iban = "NL78RABO0162136188";
+            bool expected = true;
+
+            var validator = new IbanValidator();
+
+            // Act
+            bool result = validator.Validate(iban);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void EmptyIbanShouldReturnFalse()
+        {
+            string iban = "";
+            var validator = new IbanValidator();
+
+            bool result = validator.Validate(iban);
+
+            Assert.IsFalse(result);
+        }
+    }
+}
---
+++ TddDemo/IbanValidator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TddDemo
+{
+    public class IbanValidator
+    {
+        public IbanValidator()
+        {
+        }
+
+        public bool Validate(string iban)
+        {
+            return (iban != "");
+        }
+    }
+}
