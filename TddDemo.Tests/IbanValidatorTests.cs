5f2ec030fcbc5d4de52f7b92a6ae24f11bf221e9
Manuel Riezebosch <mriezebosch@gmail.com>
2016-01-19 15:55:48 +0100

    demo met moq, fakes en shouldly

--- TddDemo.Tests/IbanValidatorTests.cs
+++ TddDemo.Tests/IbanValidatorTests.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Microsoft.QualityTools.Testing.Fakes;
+using Shouldly;
 
 namespace TddDemo.Tests
 {
@@ -84,6 +87,44 @@ namespace TddDemo.Tests
             Assert.AreEqual(expected, result);
         }
 
-        
+        [TestMethod]
+        public void IbanWithInvalidBankCodeShouldReturnFalseUsingAMockingFramework()
+        {
+            // Arrange
+            var bcv = new Mock<IBankCodeValidator>();
+            bcv.Setup(m => m.IsValidBankCode(It.IsAny<string>())).Returns(false).Verifiable();
+
+            string iban = "NL78RABO0162136188";
+            bool expected = false;
+
+            var validator = new IbanValidator(bcv.Object);
+
+            // Act
+            bool result = validator.Validate(iban);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+            result.ShouldBe(expected);
+
+            bcv.Verify();
+        }
+
+        [TestMethod]
+        public void TestWithHardDependencies()
+        {
+
+            using (ShimsContext.Create())
+            {
+                System.Fakes.ShimDateTime.TodayGet = () =>  new DateTime(2016, 04, 06);
+
+                int result = CalcAge(new DateTime(1982, 04, 05));
+                Assert.AreEqual(result, 34);
+            }
+        }
+
+        private int CalcAge(DateTime dateTime)
+        {
+            return DateTime.Today.Year - dateTime.Year;
+        }
     }
 }
