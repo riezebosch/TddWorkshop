2e57ebad9d2bdfc4e9b1c98ee4d8a2dcba63691b
Manuel Riezebosch <mriezebosch@gmail.com>
2016-02-17 20:29:04 +0100

    demo met features en alles

--- TddDemo.Tests/UnitTest1.cs
+++ TddDemo.Tests/UnitTest1.cs
@@ -7,8 +7,30 @@ namespace TddDemo.Tests
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void GeldigeIbanGeeftTrueTerug()
         {
+            // Arrange
+            string rekeningnummer = "NL78RABO0162136188";
+            bool expected = true;
+
+            var validator = new IbanValidator();
+
+            // Act
+            bool actual = validator.Validate(rekeningnummer);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void LegeIbanGeeftFalseTerug()
+        {
+            string rekeningnummer = "";
+            var validator = new IbanValidator();
+
+            var result = validator.Validate(rekeningnummer);
+
+            Assert.AreEqual(false, result);
         }
     }
 }
---
+++ TddDemo/IbanValidator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TddDemo
+{
+    public class IbanValidator
+    {
+        public bool Validate(string rekeningnummer)
+        {
+            Console.WriteLine(rekeningnummer.Length);
+
+            if (rekeningnummer != null && rekeningnummer.Length > 8)
+            {
+                Console.WriteLine(rekeningnummer[0]);
+            }
+
+            return true;
+        }
+    }
+}
