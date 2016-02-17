b57ade8ded62cadab5f88d492e62e776689cf756
Manuel Riezebosch <mriezebosch@gmail.com>
2016-02-17 10:38:41 +0100

    Eerste demo'tje met validatie op geldige en lege iban.

---
+++ TddDemo.Tests/IbanValidatorTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TddDemo.Tests
+{
+    [TestClass]
+    public class IbanValidatorTests
+    {
+        [TestMethod]
+        public void TestValidIban()
+        {
+            // Arrange
+            string input = "NL78RABO0162136188";
+            var validator = new IbanValidator();
+
+            // Act
+            bool result = validator.Validate(input);
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void TestEmptyIban()
+        {
+            // Arrange
+            string input = "";
+            var validator = new IbanValidator();
+
+            // Act
+            bool result = validator.Validate(input);
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+    }
+}
---
+++ TddDemo/IbanValidator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TddDemo
+{
+    public class IbanValidator
+    {
+        public bool Validate(string input)
+        {
+            return (input != string.Empty);
+        }
+    }
+}
