2c2e605e51e34ea927d509330f6a0cbf669d2f60
Manuel Riezebosch <mriezebosch@gmail.com>
2016-03-04 11:06:39 +0100

    Basis testje moet goed scenario en falende test voor lege input

---
+++ TddDemo.VB.Tests/UnitTest1.vb
@@ -0,0 +1,20 @@
+﻿Imports System.Text
+Imports Microsoft.VisualStudio.TestTools.UnitTesting
+
+<TestClass()> Public Class UnitTest1
+
+    <TestMethod()> Public Sub TestMethod1()
+        Dim pnc As New ProjectNummer("A1234567")
+
+        Dim result As Boolean = pnc.Check()
+
+        Assert.IsTrue(result, "Input is correct project nummer en check zou dus 'true' moeten teruggeven")
+
+    End Sub
+
+    <TestMethod()>
+    Public Sub LeegProjectNummerGeeftFalseTerug()
+        Assert.IsFalse(New ProjectNummer(Nothing).Check())
+    End Sub
+
+End Class
---
+++ TddDemo/ProjectNummer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace TddDemo
+{
+    /// <summary>
+    /// Project Nummer
+    /// </summary>
+    public class ProjectNummer
+    {
+        private string projectnummer;
+
+        /// <summary>
+        /// Initialiseert op basis van meegegeven project nummer
+        /// </summary>
+        /// <param name="projectnummer">Project nummer</param>
+        public ProjectNummer(string projectnummer)
+        {
+            this.projectnummer = projectnummer;
+        }
+
+        /// <summary>
+        /// Controleert of het projectnummer voldoet aan de ISO234234.
+        /// </summary>
+        /// <returns>True bij een geldig project nummer, anders false.</returns>
+        public bool Check()
+        {
+            if (projectnummer.Length == 8)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
