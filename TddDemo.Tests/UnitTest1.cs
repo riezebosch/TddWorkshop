c883fa5b5902574f74b75cbcfe71adaecbc66d41
Manuel Riezebosch <mriezebosch@gmail.com>
2016-05-31 11:38:49 +0200

    eerste simpele testen

--- TddDemo.Tests/UnitTest1.cs
+++ TddDemo.Tests/UnitTest1.cs
@@ -7,8 +7,44 @@ namespace TddDemo.Tests
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void InvalidEmptyIban()
         {
+            // Arrange
+            string iban = string.Empty;
+
+            // Act
+            bool result = IbanValidator.ValidateIban(iban);
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void ValidIbanReturnsTrueWhenValidating()
+        {
+            string iban = "NL78RABO0162136188";
+            Assert.IsTrue(IbanValidator.ValidateIban(iban));
+        }
+
+        [TestMethod]
+        public void GeenNederlandseIbanReturnsAltijdTrue()
+        {
+            string iban = "BE61310126985517";
+            Assert.IsTrue(IbanValidator.ValidateIban(iban));
+        }
+
+        [TestMethod]
+        public void TeKorteNederlandseIbanGeeftFalseTerug()
+        {
+            string iban = "NL78RABO016213618";
+            Assert.IsFalse(IbanValidator.ValidateIban(iban));
+        }
+
+        [TestMethod]
+        public void GeenControleCijfersInNederlandseIbanGeeftFalseTerug()
+        {
+            string iban = "NLX8RABO0162136188";
+            Assert.IsFalse(IbanValidator.ValidateIban(iban));
         }
     }
 }
---
+++ TddDemo/IbanValidator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace TddDemo
+{
+    public class IbanValidator
+    {
+        public static bool ValidateIban(string iban)
+        {
+            if (iban.StartsWith("NL"))
+            {
+                return Regex.IsMatch(iban, @"NL\d{2}.{14}");
+            }
+            else
+            {
+                return Regex.IsMatch(iban, "[A-Z]{2}.*");
+            }
+        }
+    }
+}
