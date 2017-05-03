03e91d9474cc413634d7a87bc80a2ca2c36e1352
Manuel Riezebosch <mriezebosch@gmail.com>
2017-05-03 13:28:53 +0200

    refactor: TestClass1 heet tegenwoordig IbanValidatorTests

--- TddDemo.Tests/IbanValidatorTests.cs
+++ TddDemo.Tests/IbanValidatorTests.cs
@@ -4,7 +4,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace TddDemo.Tests
 {
     [TestClass]
-    public class UnitTest1
+    public class IbanValidatorTests
     {
         [TestMethod]
         public void GivenValidIban_WhenValidate_ThenResultIsTrue()
