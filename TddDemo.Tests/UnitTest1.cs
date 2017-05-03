9f3f527cb849972ef9b972c5f796ab63ab6327da
Manuel Riezebosch <mriezebosch@gmail.com>
2017-05-03 10:05:37 +0200

    allereerste unit test op geldige iban groen!

--- TddDemo.Tests/UnitTest1.cs
+++ TddDemo.Tests/UnitTest1.cs
@@ -9,6 +9,15 @@ namespace TddDemo.Tests
         [TestMethod]
         public void TestMethod1()
         {
+            // Arrange
+            string input = "NL74 INGB 0671 5336 65";
+            bool expected = true;
+
+            // Act
+            bool actual = true;
+
+            // Assert
+            Assert.AreEqual(expected, actual);
         }
     }
 }
