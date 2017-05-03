c6e12484a1ea737dbadfdb89e0879593713a4732
Manuel Riezebosch <mriezebosch@gmail.com>
2017-05-03 15:23:51 +0200

    specflow!

---
+++ TddDemo.Tests/IBANValidationSteps.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using TechTalk.SpecFlow;
+
+namespace TddDemo.Tests
+{
+    [Binding]
+    public class IBANValidationSteps
+    {
+        [Given(@"I have entered ""(.*)"" into the validator")]
+        public void GivenIHaveEnteredIntoTheValidator(string iban)
+        {
+            
+            ScenarioContext.Current.Set(iban, "iban");
+        }
+
+        [When(@"I execute Validate")]
+        public void WhenIExecuteValidate()
+        {
+            var iban = ScenarioContext.Current.Get<string>("iban");
+            var validator = new IbanValidator(new BankCodeProvider());
+
+            var result = validator.ValidateIban(iban);
+            ScenarioContext.Current.Set(result, "result");
+        }
+        
+        [Then(@"the result should be (.*)")]
+        public void ThenTheResultShouldBeTrue(bool expected)
+        {
+            var result = ScenarioContext.Current.Get<bool>("result");
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
---
+++ TddDemo.Tests/SpecFlowFeature1.feature
@@ -0,0 +1,10 @@
+﻿Feature: IBAN Validation
+	In order to avoid silly mistakes
+	As a math idiot
+	I want to be told if my IBAN is valid
+
+@mytag
+Scenario: Valid IBAN
+	Given I have entered "NL74 INGB 0671 5336 65" into the validator
+	When I execute Validate
+	Then the result should be true
