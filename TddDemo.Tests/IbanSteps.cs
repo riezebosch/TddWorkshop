facb35217940924d632d21c6741e5279948d5fa6
Manuel Riezebosch <mriezebosch@gmail.com>
2017-09-21 17:33:45 +0200

    lege specflow test

---
+++ TddDemo.Tests/IbanSteps.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace TddDemo.Tests
+{
+    [Binding]
+    public class IbanSteps
+    {
+        [Given(@"I have entered (.*) into the calculator")]
+        public void GivenIHaveEnteredIntoTheCalculator(int input)
+        {
+            
+        }
+        
+        [When(@"I press add")]
+        public void WhenIPressAdd()
+        {
+            
+        }
+        
+        [Then(@"the result should be (.*) on the screen")]
+        public void ThenTheResultShouldBeOnTheScreen(int expected)
+        {
+            
+        }
+    }
+}
