f51c765c8d1753dc60056f952e914032a54c257b
Manuel Riezebosch <mriezebosch@gmail.com>
2019-11-14 15:20:35 +0100

    update: netcore2

--- TddDemo.Tests/UnitTest1.cs
+++ TddDemo.Tests/UnitTest1.cs
@@ -1,12 +1,11 @@
 ﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Xunit;
 
 namespace TddDemo.Tests
 {
-    [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
+        [Fact]
         public void TestMethod1()
         {
         }
